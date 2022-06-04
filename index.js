393416ec87ef18cecc68ded2134115a80a9b3184
Hyungmo Gu <guhyungm7@gmail.com>
2022-06-03 20:16:27 -0600

    Add main code

---
+++ index.js
@@ -0,0 +1,29 @@
+exports.htmlToXpath = function(htmlElement) {
+  let res = "";
+  let current = htmlElement
+
+  if (htmlElement instanceof HTMLElement) {
+    throw new Error("[Error]: The entered argument is not HTMLElement. Please add HTMLElement element from visible DOM Tree. (i.e. document.querySelector(...) or document.getElementById(...)).");
+  }
+
+  if (htmlElement.closest("html") === null) {
+    throw new Error("[Error]: This element is not a part of visible dom. Please add element NOT from document.createElement.");
+  }
+
+  // check if element is part of full DOM tree
+  while (current.tagName.toLowerCase() !== "html") {
+    // find nth children this element is located, translate this to xpath and add to res
+    const index = [...current.parentElement.children].findIndex(x => x.isSameNode(current));
+
+    if (current.parentElement.tagName.toLowerCase() === "html") {
+      res = `//html/${current.tagName.toLowerCase()}[${index+1}]` + res
+    } else {
+      res = `/${current.tagName.toLowerCase()}[${index+1}]` + res
+    }
+
+    // update current element to it's parent
+    current = current.parentElement;
+  }
+
+  return res;
+}
